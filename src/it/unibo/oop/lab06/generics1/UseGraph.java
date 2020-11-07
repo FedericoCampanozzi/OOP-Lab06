6e208f74624eff76c932ffee704c39bd9ae4014c
federico.campanozzi@gmail.com <federico.campanozzi@studio.unibo.it>
2020-11-07 20:47:28 +0100

    Aggiunti test case con JUnit nell' esercizio 6

--- src/it/unibo/oop/lab06/generics1/UseGraph.java
+++ src/it/unibo/oop/lab06/generics1/UseGraph.java
@@ -1,46 +1,37 @@
 package it.unibo.oop.lab06.generics1;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
 /**
  *
  */
 public final class UseGraph {
-
-    private UseGraph() {
-    }
-
-    /**
-     * @param args
-     *            ignored
-     */
-    public static void main(final String... args) {
-        final Graph<String> g = new GraphImpl<>();
-        g.addNode("a");
-        g.addNode("b");
-        g.addNode("c");
-        g.addNode("d");
-        g.addNode("e");
-        g.addNode("f");
-        g.addEdge("a", "b");
-        g.addEdge("b", "c");
-        g.addEdge("c", "d");
-        g.addEdge("d", "e");
-        g.addEdge("c", "a");
-        g.addEdge("e", "a");
-        g.addEdge("f", "e");
-        
-        System.out.println(g.toString());
-        
-        /*
-         * Should print ["a","b","c","d","e"], in any order
-         */
-        System.out.println("Set di nodi = " + g.nodeSet());
-        /*
-         * ["d","a"], in any order
-         */
-        System.out.println("Nodi per c = " + g.linkedNodes("c"));
-        /*
-         * Must print either the path b,c,a or b,c,d,e,a
-         */
-        System.out.println("Percorso da [b] -> [e] = " + g.getPath("f", "e"));
+	
+    @Test
+    public void testGraph() {
+    	 
+    	final Graph<String> g = new GraphImpl<>();
+         g.addNode("a");
+         g.addNode("b");
+         g.addNode("c");
+         g.addNode("d");
+         g.addNode("e");
+         g.addNode("f");
+         g.addEdge("a", "b");
+         g.addEdge("b", "c");
+         g.addEdge("c", "d");
+         g.addEdge("d", "e");
+         g.addEdge("c", "a");
+         g.addEdge("e", "a");
+         g.addEdge("f", "e");
+         
+         assertEquals("List nodeSet() are not equal",g.nodeSet(), Set.of("a","b","c","d","e", "f"));
+         assertEquals("List linkedNodes(\"c\") are not equal",g.linkedNodes("c"), Set.of("a","d"));
+         assertEquals("List getPath(\"f\", \"e\") are not equal",g.getPath("a", "e"), List.of("a","b","c","d","e"));
     }
 }
